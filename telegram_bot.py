f4184ba88525b752ab1d2a1365318d4c95e4bdd5
a83663900-ops <a83663900@gmail.com>
2026-06-15 03:23:26 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -19,7 +19,7 @@ PROMPT_FILE = "system-prompt.txt"
 USER_LANG_FILE = "user_langs.json"
 
 MODEL_CONFIG = {
-    "name": "tngtech/deepseek-r1t2-chimera:free",
+    "name": "meta-llama/llama-3-8b-instruct:free",
     "base_url": "https://openrouter.ai/api/v1",
     "key": os.getenv("OPENROUTER_KEY"),
 }
